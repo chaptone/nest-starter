cbd211b4a3b8a8c37dba5fb1192ebb7b7c76165a
chaptone <rakkan.j@gmail.com>
2020-05-30 02:08:58 +0700

    Add swagger

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const configService = app.get(ConfigService);
   const port = configService.get('port')
+
+  const options = new DocumentBuilder()
+    .setTitle('Contract List App')
+    .setDescription('Contract List App API description')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(port);
+  console.log('Server running at port: ' + port)
 }
 bootstrap();
