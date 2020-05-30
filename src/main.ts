e78a66f05d010f2b4a52f6c0f681fe2580be58b2
chaptone <rakkan.j@gmail.com>
2020-05-30 17:53:11 +0700

    Add security

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,22 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppConfigService } from './config/app/app.config.service';
+import * as helmet from 'helmet';
+import * as csurf from 'csurf';
+import * as rateLimit from 'express-rate-limit';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const appConfigService : AppConfigService = app.get('AppConfigService');
+  app.use(helmet());
+  app.enableCors();
+  app.use(
+    rateLimit({
+      windowMs: 15 * 60 * 1000, // 15 minutes
+      max: 100, // limit each IP to 100 requests per windowMs
+    }),
+  );
+  // app.use(csurf());
 
   const options = new DocumentBuilder()
     .setTitle('Contract List App')
