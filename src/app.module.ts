febe8c95cb066a9381dfbcc21d5e321ef9854932
chaptone <rakkan.j@gmail.com>
2020-05-30 17:55:34 +0700

    Delete unnecessary

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,9 @@
 import { Module } from '@nestjs/common'
 import { AppController } from './app.controller'
-import { AppService } from './app.service'
 import { AppConfigModule } from './config/app/app.config.module';
 import { AuthModule } from './auth/auth.module';
 import { UsersService } from './users/users.service';
 import { UsersModule } from './users/users.module';
-import { JwtConfigModule } from './config/jwt/jwt.config.module';
 
 @Module({
   imports: [
@@ -14,6 +12,6 @@ import { JwtConfigModule } from './config/jwt/jwt.config.module';
     UsersModule,
   ],
   controllers: [AppController],
-  providers: [AppService, UsersService],
+  providers: [UsersService],
 })
 export class AppModule {}
